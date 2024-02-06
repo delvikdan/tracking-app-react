1e165f235c96088b1135b2b6d6853fdc57888eca
Delvik Danielov <delvikdan@gmail.com>
2024-02-06 14:06:54 +0300

    Added pages and navigation component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../component/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../component/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Not Found</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
