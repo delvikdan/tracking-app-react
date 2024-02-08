1225c3b065ee5543bbe1d1fa9026cd659b6a67a4
Delvik Danielov <delvikdan@gmail.com>
2024-02-08 18:25:49 +0300

    Nested Routes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,11 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <>
-      <footer className={styles.footer}></footer>
+    <footer className={styles.footer}>
       <p className={styles.copyright}>
         &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
       </p>
-    </>
+    </footer>
   );
 }
 
